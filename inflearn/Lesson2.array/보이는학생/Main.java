c73e5be38bbc477764e2ef617748181025f8386c
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-06-09 00:21:42 +0900

    [220608] array - 입력 방식 변경

--- inflearn/Lesson2.array/보이는학생/Main.java
+++ inflearn/Lesson2.array/보이는학생/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class Main {
@@ -6,9 +5,28 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         Scanner kb = new Scanner(System.in);
-        int count = kb.nextInt();
-        String str = kb.next();
-        System.out.println(main.solution(count, str));
+        int count = kb.nextInt();        
+//        String str = kb.nextLine();
+//        System.out.println(main.solution(count, str));
+        int[] highs = new int[count];
+        for (int i = 0; i < count; i++) {
+            highs[i] = kb.nextInt();
+        }
+        System.out.println(main.solution(count, highs));
+    }
+
+    public int solution(int count, int[] highs) {
+        int answer = 1;
+        int max = highs[0];
+        for (int i = 0; i < count; i++) {
+            int studentHigh = highs[i];
+            if (studentHigh > max)
+            {
+                answer++;
+                max = studentHigh;
+            }
+        }
+        return answer;
     }
 
     // 런타임 에러
--- inflearn/Lesson2.array/큰수출력/Main.java
+++ inflearn/Lesson2.array/큰수출력/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -6,11 +7,17 @@ public class Main {
         Main main = new Main();
         Scanner kb = new Scanner(System.in);
         int count = kb.nextInt();
-        String str = kb.next();
-        System.out.println(main.solution(count, str));
+        int[] numbers = new int[count];
+        for (int i = 0; i < count; i++) {
+            numbers[i] = kb.nextInt();
+        }
+        ArrayList<Integer> answers = main.solution(count, numbers);
+        for (Integer answer : answers) {
+            System.out.print(answer + " ");
+        }
     }
 
-    // 런타임 에러
+    // 런타임 에러 - 사실 잘 모르겠다. 도대체! 기준을.. 출력 방식을 바꿔주면 된다.
     public String solution(int count, String str) {
         StringBuilder sb = new StringBuilder();
         String[] numbers = str.split(" ");
@@ -32,5 +39,26 @@ public class Main {
             }
         }
         return sb.toString();
+    }
+
+    public ArrayList<Integer> solution(int count, int[] numbers) {
+        ArrayList<Integer> answers = new ArrayList<Integer>();
+
+        for (int i = 0; i < count; i++) {
+            int number = numbers[i];
+            int preNumber;
+            if (i == 0)
+            {
+                preNumber = 0;
+            }
+            else
+            {
+                preNumber = numbers[i - 1];
+            }
+            if (number > preNumber) {
+                answers.add(number);
+            }
+        }
+        return answers;
     }
 }
