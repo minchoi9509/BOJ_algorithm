ba0e2d35c7824ebbddb8aff125d7d3f2881c0d1a
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-07-26 23:59:58 +0900

    [220726] 삽입정렬

--- src/main/java/com/inflearn/정렬/LRU/Main.java
+++ src/main/java/com/inflearn/정렬/LRU/Main.java
@@ -1,7 +1,5 @@
 package com.inflearn.정렬.LRU;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -15,22 +13,33 @@ public class Main {
         for (int i = 0; i < s; i++) {
             jobs[i] = kb.nextInt();
         }
-        Queue<Integer> answers = main.solution(n, jobs);
+        int[] answers = main.solution(s, n, jobs);
         for (int answer : answers) {
             System.out.print(answer + " ");
         }
     }
 
-    public Queue<Integer> solution(int n, int[] jobs) {
-        Queue<Integer> answer = new LinkedList<>();
-        for (int i = 0; i < jobs.length; i++) {
-            if (answer.contains(jobs[i])) {
-                answer.remove(jobs[i]);
-            } else if (answer.size() < n) {
-                answer.offer(jobs[i]);
-            } else if (answer.size() == n) {
-                answer.poll();
+    public int[] solution(int s, int n, int[] jobs) {
+        int[] answer = new int[s];
+        for (int job : jobs) {
+            int position = -1;
+            for (int i = 0; i < s; i++) {
+                if (job == answer[i]) {
+                    position = i;
+                }
             }
+            if (position == -1) {
+                for (int i = s - 1; i >= 1 ; i--) {
+                    answer[i] = answer[i - 1];
+                }
+            }
+            else {
+                for (int i = position; i >= 1 ; i--) {
+                    answer[i] = answer[i - 1];
+                }
+            }
+
+            answer[0] = job;
         }
         return answer;
     }
