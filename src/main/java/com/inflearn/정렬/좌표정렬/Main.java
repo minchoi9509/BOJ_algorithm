da891b4d2335b5c0b6ce9a184ec9c3c99c6c348b
minchoi <53928609+minchoi9509@users.noreply.github.com>
2022-08-18 23:53:06 +0900

    [220818] 좌표정렬

--- src/main/java/com/inflearn/정렬/좌표정렬/Main.java
+++ src/main/java/com/inflearn/정렬/좌표정렬/Main.java
@@ -1,26 +1,45 @@
 package com.inflearn.정렬.좌표정렬;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Main main = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[][] numbers = new int[n][2];
+        ArrayList<Point> points = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            numbers[i][0] = kb.nextInt();
-            numbers[i][1] = kb.nextInt();
+            int x = kb.nextInt();
+            int y = kb.nextInt();
+            points.add(new Point(x, y));
         }
-        System.out.println(main.solution(n, numbers));
+
+        Collections.sort(points);
+
+        for (Point point : points) {
+            System.out.print(point.x + " " + point.y);
+            System.out.println();
+        }
+    }
+}
+
+class Point implements Comparable<Point> {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public int[][] solution(int n, int[][] numbers) {
-        for (int i = 0; i <n; i++) {
-
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) {
+            // 오름차순
+            return this.y - o.y;
         }
-        return numbers;
-
+        return this.x - o.x;
     }
 }
